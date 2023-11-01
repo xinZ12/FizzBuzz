3954f3b2f71075a88abb00749878ec00ab0a28d1
Xin <xzhao34@slb.com>
2023-11-01 14:15:25 +0800

    feat: add CountOff method

--- FizzBuzz/FizzBuzzGame.cs
+++ FizzBuzz/FizzBuzzGame.cs
@@ -1,6 +1,28 @@
-﻿namespace FizzBuzz
+﻿using System.Threading.Tasks;
+
+namespace FizzBuzz
 {
     public class FizzBuzzGame
     {
+        public string CountOff(int input)
+        {
+            if (input % 3 == 0)
+            {
+                if (input % 5 == 0)
+                {
+                    return "FizzBuzz";
+                }
+
+                return "Fizz";
+            }
+            else if (input % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else
+            {
+                return input.ToString();
+            }
+        }
     }
 }
