822e972d6fa94e532dec78303347189c06fef837
Xin <xzhao34@slb.com>
2023-11-01 14:14:21 +0800

    test: add test for normal case and multiple cases

--- FizzBuzzTest/FizzBuzzGameTest.cs
+++ FizzBuzzTest/FizzBuzzGameTest.cs
@@ -6,12 +6,61 @@ namespace FizzBuzzTest
     public class FizzBuzzGameTest
     {
         [Fact]
-        public void Test1()
+        public void Should_return_number_when_give_normal_case()
         {
+            // given
             FizzBuzzGame game = new FizzBuzzGame();
+            const int input = 1;
+
+            //when
+            string output = game.CountOff(input);
 
             //then
-            Assert.NotNull(game);
+            //Assert.NotNull(game);
+            Assert.Equal("1", output);
+        }
+
+        [Fact]
+        public void Should_return_Fizz_when_give_multipleOf3()
+        {
+            // given
+            FizzBuzzGame game = new FizzBuzzGame();
+            const int input = 3;
+
+            //when
+            string output = game.CountOff(input);
+
+            //then
+            //Assert.NotNull(game);
+            Assert.Equal("Fizz", output);
+        }
+
+        [Fact]
+        public void Should_return_Buzz_when_give_multipleOf5()
+        {
+            // given
+            FizzBuzzGame game = new FizzBuzzGame();
+            const int input = 5;
+
+            //when
+            string output = game.CountOff(input);
+
+            //then
+            Assert.Equal("Buzz", output);
+        }
+
+        [Fact]
+        public void Should_return_FizzBuzz_when_give_multipleOf3And5()
+        {
+            // given
+            FizzBuzzGame game = new FizzBuzzGame();
+            const int input = 15;
+
+            //when
+            string output = game.CountOff(input);
+
+            //then
+            Assert.Equal("FizzBuzz", output);
         }
     }
 }
